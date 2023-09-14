dd1b75916b5b857dfdf2d665d06f66fe253c6eea
Evan Yang <71720197+Evany226@users.noreply.github.com>
2023-09-14 11:34:14 -0400

    home page revamp

--- trello/src/App.jsx
+++ trello/src/App.jsx
@@ -1,6 +1,7 @@
 import './css/App.css'
 import Button from "./components/Button.jsx"
 
+
 function App() {
 
   return (
--- trello/src/main.jsx
+++ trello/src/main.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Home from './homepage.jsx'
 import './css/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+    <Home/>
 )
