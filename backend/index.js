72f4aa8e4e128cd03526bdfe3ab8b8e95a0e0a6e
Evan Yang <71720197+Evany226@users.noreply.github.com>
2023-10-17 22:50:33 -0400

    Revert "refactored, next auth0"
    This reverts commit d902c31a7541e0629f9e7588a685ebec6a1c62e8.

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,122 @@
-const app = require("./app"); // the actual Express application
-const config = require("./utils/config");
-const logger = require("./utils/logger");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const Note = require("./models/backendNote");
 
-app.listen(config.PORT, () => {
-  logger.info(`Server running on port ${config.PORT}`);
+app.use(cors());
+app.use(express.static("dist"));
+const mongoose = require("mongoose");
+
+app.use(express.json());
+const requestLogger = (request, response, next) => {
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  next();
+};
+
+app.use(requestLogger);
+
+let notes = [
+  {
+    name: "College",
+    content: [
+      {
+        taskItem: "Finishing college applications",
+        id: 1,
+      },
+      {
+        taskItem: "Completing my homework",
+        id: 2,
+      },
+      {
+        taskItem: "Cleaning house",
+        id: 3,
+      },
+      {
+        taskItem: "Applying Scholarships",
+        id: 4,
+      },
+    ],
+    id: 1,
+  },
+  {
+    name: "College",
+    content: [
+      {
+        taskItem: "Finishing college applications",
+        id: 1,
+      },
+    ],
+    id: 2,
+  },
+];
+
+app.get("/", (request, response) => {
+  response.send("<h1>The app is working</h1>");
+});
+
+app.get("/api/notes", (request, response) => {
+  Note.find({}).then((notes) => {
+    response.json(notes);
+  });
+});
+
+app.get("/api/notes/:id", (request, response) => {
+  Note.findById(request.params.id).then((note) => {
+    response.json(note);
+  });
+});
+
+app.delete("/api/notes/:id", (request, response) => {
+  Note.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
+});
+
+app.post("/api/notes", (request, response) => {
+  const body = request.body;
+
+  if (body.name === undefined) {
+    return response.status(400).json({ error: "name missing" });
+  }
+
+  const note = new Note({
+    name: body.name,
+    content: body.content || [],
+  });
+
+  note.save().then((savedNote) => {
+    response.json(savedNote);
+  });
+});
+
+app.put("/api/notes/:id", (request, response) => {
+  const body = request.body;
+
+  const note = {
+    name: body.name,
+    content: body.content,
+  };
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then((updatedNote) => {
+      response.json(updatedNote);
+    })
+    .catch((error) => next(error));
+});
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
