aaa75302341696313b708e140237a9ba42a2e7b1
Evan Yang <71720197+Evany226@users.noreply.github.com>
2023-12-28 21:27:56 -0500

    finished homepage

--- trello/src/pages/HomePage.jsx
+++ trello/src/pages/HomePage.jsx
@@ -3,6 +3,7 @@ import "../css/Button.css";
 import "../css/index.css";
 import SignupButton from "../components/SignupButton.jsx";
 import LoginButton from "../components/LoginButton.jsx";
+import TryButton from "../components/TryButton.jsx";
 import { ReactComponent } from "../assets/new.svg";
 
 function Home() {
@@ -10,12 +11,9 @@ function Home() {
     <>
       <section id="background">
         <nav className="homeNav">
-          <div className="navAdjust">
-            <div id="titleWrapper">
-              <h3 className="title"> dragn</h3>
-              <h3 className="title-2">drop </h3>
-            </div>
-            <h1 class="center-text">Test</h1>
+          <div id="titleWrapper">
+            <h3 className="title"> dragn</h3>
+            <h3 className="title-2">drop </h3>
           </div>
           <div className="buttonWrapper">
             <LoginButton buttonName="Log In" />
@@ -25,15 +23,19 @@ function Home() {
 
         <div id="foreground">
           <div className="mainTextWrapper">
-            <ReactComponent style={{ width: "90%" }} />
+            <ReactComponent style={{ width: "80%" }} />
           </div>
           <div className="subTextWrapper">
-            <h2 className="main-title">DragnDrop</h2>
-            <h5 className="sub-title">
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum is simply dummy text of the printing and
-              typesetting industry.
-            </h5>
+            <div className="slogan-wrapper">
+              <h2 className="main-title">Organize your life!</h2>
+              <h5 className="sub-title">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum is simply dummy text of the printing and
+                typesetting industry. Lorem Ipsum is simply dummy text of the
+                printing and typesetting industry.
+              </h5>
+              <TryButton buttonName="Try for free" />
+            </div>
           </div>
         </div>
       </section>
