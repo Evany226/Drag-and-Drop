c50a89cecb7a00ad87dae63ca5277f44e0d1091d
Evan Yang <71720197+Evany226@users.noreply.github.com>
2024-03-15 22:59:10 -0400

    changes

--- trello/src/components/Note.jsx
+++ trello/src/components/Note.jsx
@@ -5,7 +5,7 @@ import DeleteDropdown from "./DeleteDropdown.jsx";
 import NoteButton from "./NoteButton.jsx";
 import NoteItem from "./NoteItem.jsx";
 import { useState } from "react";
-import { BsThreeDots } from "react-icons/bs";
+import { ReactComponent as ThreeDots } from "../assets/dots.svg";
 import { Draggable } from "@hello-pangea/dnd";
 import { Droppable } from "@hello-pangea/dnd";
 
@@ -88,7 +88,7 @@ const Note = ({
                 </div>
 
                 <div className="settings-wrapper">
-                  <BsThreeDots
+                  <ThreeDots
                     style={{
                       width: "35%",
                       cursor: "pointer",
